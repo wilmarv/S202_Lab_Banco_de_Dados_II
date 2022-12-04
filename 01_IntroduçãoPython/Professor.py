92b1526b5db2864bf15cadd72f5430c581d7ebb7
wilmar vitor fonseca <wilmarfonseca@gec.inatel.br>
2022-12-03 22:47:42 -0300

    Iniciando Branch ProjetoBancoDadosII

--- 01_IntroduçãoPython/Professor.py
+++
@@ -1,9 +0,0 @@
-from Pessoa import Pessoa
-
-class Professor(Pessoa):
-    def __init__(self,nome,especialidade):
-        super().__init__(nome)
-        self.especialidade = especialidade
-
-    def __str__(self) -> str:
-        return f"{self.nome}"
